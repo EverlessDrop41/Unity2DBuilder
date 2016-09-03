a4ad8c416e40ac22f5ade10d1c68b4be13bd0d3e
Peremily <tomfperegrine@googlemail.com>
2016-09-03 12:02:46 +0100

    Made camera movement speed relative

--- Assets/Scripts/Controllers/CameraControl.cs
+++ Assets/Scripts/Controllers/CameraControl.cs
@@ -13,9 +13,12 @@ namespace TwoDBuilder.Controllers
         public float MaxCameraSize = 30f;
         public const float MinCameraSize = 1f;
 
+        private float _relativeMoveSpeed;
+
         // Use this for initialization
         void Start()
         {
+            _relativeMoveSpeed = MoveSpeed;
             if (GameCamera == null)
             {
                 GameCamera = Camera.main;
@@ -25,17 +28,19 @@ namespace TwoDBuilder.Controllers
         // Update is called once per frame
         void Update()
         {
-            float horzInpt = Input.GetAxis("Horizontal");
-            float vertInpt = Input.GetAxis("Vertical");
-
-            float horzMove = horzInpt * MoveSpeed;
-            float vertMove = vertInpt * MoveSpeed;
-
-            GameCamera.transform.position += new Vector3(horzMove * Time.deltaTime, vertMove * Time.deltaTime);
-
             float zoomInput = Input.GetAxis("CameraZoom");
             GameCamera.orthographicSize += zoomInput * ZoomSpeed;
             GameCamera.orthographicSize = Mathf.Clamp(GameCamera.orthographicSize, MinCameraSize, MaxCameraSize);
+
+            _relativeMoveSpeed = MoveSpeed * GameCamera.orthographicSize / 2;
+
+            float horzInpt = Input.GetAxis("Horizontal");
+            float vertInpt = Input.GetAxis("Vertical");
+
+            float horzMove = horzInpt * _relativeMoveSpeed;
+            float vertMove = vertInpt * _relativeMoveSpeed;
+
+            GameCamera.transform.position += new Vector3(horzMove * Time.deltaTime, vertMove * Time.deltaTime);
         }
     }
 }
